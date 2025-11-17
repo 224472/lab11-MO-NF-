054c5410f4363b9654e9cb54f12358239b69422a
224472 <shanou0601@gmail.com>
2025-11-16 23:59:51 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -12,15 +12,15 @@ import math
 
 def add(a, b): 
     return a+b
-def sub(a, b): 
+def subtract(a, b): 
     return a - b
 def mul(a, b):
     return a * b
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid logarithm arguments")
     return math.log(b, a)
