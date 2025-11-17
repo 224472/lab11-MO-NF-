df9bb03249b29a44446ebb0392e713ca24de927e
224472 <shanou0601@gmail.com>
2025-11-16 23:50:16 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,6 +5,10 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+
+# https://github.com/224472/lab11-MO-NF-
+# Partner 1: [Michael Ou]
+# Partner 2: [Partner finished with someone else]
 def add(a, b): 
     return a+b
 def sub(a, b): 
